4d5041ce20631ada842646012ff1c5d8c42e80e2
goerkem.demirkol <goerkem.demirkol@regnology.net>
2022-02-14 13:18:34 +0100

    view encapsulation added

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,22 +1,18 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.css']
+  styleUrls: ['./favorite.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
-export class FavoriteComponent implements OnInit {
+export class FavoriteComponent {
   @Input('isFavorite') isFavorite: boolean = false;
   @Output() change = new EventEmitter();
   // input decorator can be used without supplying an argument, then it takes
   // the name of the variable. However 'isFavorite' magic string in input 
   // decorator is useful in this case beacause if we change variable name our
   // app will not be efected from it.
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
   onClick(){
     this.isFavorite=!this.isFavorite;
