4f1a6cff7ec2e2579e74c2231b241e8612a1aff5
Demirkol, Goerkem <gdemirkol315@gmail.com>
2022-02-19 20:32:35 +0100

    Forms implemented

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent implements OnInit {
+export class ContactFormComponent {
+  minLength: number = 3;
+  chosenContactMethod: number;
+  contactMethods = [{id: 1, name: 'E-mail'},
+    {id: 2, name: 'SMS'},
+    {id: 3, name: 'Phone call'}];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  submit(f: any) {
+    console.log(f);
+    console.log(f.value);
+    // f.value is a json representation of our form
+    // we can send this to an API
   }
-
 }
