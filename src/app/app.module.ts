15d9b6cde7152261b1bfc086aaaef31acc950be2
goerkem.demirkol <goerkem.demirkol@regnology.net>
2022-03-04 10:50:53 +0100

    project refactored for services&pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {CoursesComponent} from './courses.component';
 import {CourseComponent} from './course/course.component';
-import {CoursesService} from './courses.service';
+import {CoursesService} from './services/courses.service';
 import {FavoriteComponent} from './favorite/favorite.component';
 import {InputComponent} from './input/input.component';
-import {TitleCasePipe} from './title-case.pipe';
+import {TitleCasePipe} from './common/pipes/title-case.pipe';
 import {PanelComponent} from './panel/panel.component';
 import {LikeComponent} from './like/like.component';
 
@@ -18,17 +18,17 @@ import {ZippyComponent} from './zippy/zippy.component';
 import {PanelSwitchComponent} from './panel-switch/panel-switch.component';
 import {ContactFormComponent} from './contact-form/contact-form.component';
 import {FormInputComponent} from './form-input/form-input.component';
-import {LowercasePipe} from './lowercase.pipe';
+import {LowercasePipe} from './common/pipes/lowercase.pipe';
 import {SignupFormComponent} from "./signup-form/signup-form.component";
 import {NewCourseFormComponent} from './new-course-form/new-course-form.component';
 import {PostsComponent} from './posts/posts.component';
 import {HttpClientModule} from "@angular/common/http";
+import {PostService} from "./services/post.service";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent,
     CourseComponent,
     FavoriteComponent,
     InputComponent,
@@ -55,7 +55,8 @@ import {HttpClientModule} from "@angular/common/http";
     //dependency injection - singleton pattern
     //there will be one CoursesService instance which will be used
     //everywhere where it is called in our app
-    CoursesService
+    CoursesService,
+    PostService
   ],
   bootstrap: [AppComponent]
 })
